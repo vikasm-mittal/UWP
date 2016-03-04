9250328e2f3d8c2e7d115aac4d1e273406283640
Vikas Mittal <vikasm_mittal@hotmail.com>
2016-03-04 11:05:37 -0500

    UWP projects

---
+++ HelloMVVMLight/HelloMVVMLight/Model/IDataService.cs
@@ -0,0 +1,9 @@
+﻿using System.Threading.Tasks;
+
+namespace HelloMVVMLight.Model
+{
+    public interface IDataService
+    {
+        Task<DataItem> GetData();
+    }
+}
---
+++ Jewelry/Jewelry/Model/IDataService.cs
@@ -0,0 +1,9 @@
+﻿using System.Threading.Tasks;
+
+namespace Jewelry.Model
+{
+    public interface IDataService
+    {
+        Task<DataItem> GetData();
+    }
+}
